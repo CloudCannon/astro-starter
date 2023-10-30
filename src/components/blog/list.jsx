645ffe63535d7961c2a679f7d009cbf270d7d8cb
avping <avping@users.noreply.github.com>
2023-10-30 15:06:47 +0100

    Update list.jsx
    fix className typo

--- src/components/blog/list.jsx
+++ src/components/blog/list.jsx
@@ -9,7 +9,7 @@ export default function BlogList({ posts, page }) {
   }
   if (posts.length > 0 && page.slug === 'index') {
     return (
-      <section class="container mx-auto px-3">
+      <section className="container mx-auto px-3">
         <div className="grid lg:grid-cols-2 2xl:grid-cols-3 justify-center gap-12 mt-8 md:mt-16">
           {posts.map((post, i) => (
             <PostSummary post={post} key={i} />
@@ -22,7 +22,7 @@ export default function BlogList({ posts, page }) {
     );
   } else if (posts.length > 0) {
     return (
-      <section class="container mx-auto px-3">
+      <section className="container mx-auto px-3">
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold text-center my-12">
           {title}
         </h1>
