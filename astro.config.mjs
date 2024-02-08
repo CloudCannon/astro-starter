9f1a806ba965c0ff7182fc16a1552efa874fdaac
tomrcc <tom@cloudcannon.com>
2024-02-09 10:38:52 +1300

    Try add assets experimental to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    assets: true,
+  },
   site: 'https://muggy-fish.cloudvent.net/',
   integrations: [react(), tailwind(), bookshop()],
 });
