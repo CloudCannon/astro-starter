69572d4469564508b967a0b3f5a7d05e83951910
tomrcc <tom@cloudcannon.com>
2024-04-04 14:34:20 +1300

    Populate header and footer via data file

--- src/components/navigation/header.jsx
+++ src/components/navigation/header.jsx
@@ -1,6 +1,8 @@
+import { header } from '../../../data/navigation.json'; 
+
 export default function Header() {
   return (
-    <header className="max-w-[1280px] mx-auto header px-6 md:px-12">
+    <header className="max-w-[1280px] mx-auto header px-6 md:px-12 flex justify-between">
       <a href="/" rel="home">
         <img
           className="logo header-logo"
@@ -8,6 +10,17 @@ export default function Header() {
           alt=""
         />
       </a>
+      <ul className='flex gap-5'>
+        {header.links.map((link, index) => {
+          return (
+            <li key={index}>
+              <a href={link.link}>
+                {link.text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
     </header>
   );
 }
