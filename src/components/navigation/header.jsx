f22591eb1467866ccf3b79dee1276bd15dbbaa3f
tomrcc <tom@cloudcannon.com>
2024-04-09 12:18:03 +1200

    Fix navigation

--- src/components/navigation/header.jsx
+++ src/components/navigation/header.jsx
@@ -1,22 +1,16 @@
-import { header } from '../../../data/navigation.json'; 
+import { header } from '../../../data/navigation.json';
 
 export default function Header() {
   return (
     <header className="max-w-[1280px] mx-auto header px-6 md:px-12 flex justify-between">
-      <a href={header.logo.logo_url}>
-        <img
-          className="logo header-logo"
-          src={header.logo}
-          alt={header.logo}
-        />
+      <a href={header.logo_url}>
+        <img className="logo header-logo" src={header.logo} alt={header.logo} />
       </a>
-      <ul className='flex gap-5'>
+      <ul className="flex gap-5">
         {header.links.map((link, index) => {
           return (
             <li key={index}>
-              <a href={link.link}>
-                {link.text}
-              </a>
+              <a href={link.link}>{link.text}</a>
             </li>
           );
         })}
