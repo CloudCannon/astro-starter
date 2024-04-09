b50819a1b1ed9f71b42341dd3504384b125961e9
tomrcc <tom@cloudcannon.com>
2024-04-09 12:30:51 +1200

    Change md to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import bookshop from '@bookshop/astro-bookshop';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://muggy-fish.cloudvent.net/',
-  integrations: [react(), tailwind(), bookshop()],
+  integrations: [mdx(), react(), tailwind(), bookshop()],
 });
