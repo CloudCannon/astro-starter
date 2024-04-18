23822faf1dc3494093e9be31035e1143dc01a498
tomrcc <tom@cloudcannon.com>
2024-04-19 11:19:08 +1200

    Working on navigation, tidy ups

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import react from '@astrojs/react';
 import bookshop from '@bookshop/astro-bookshop';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import alpine from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://muggy-fish.cloudvent.net/',
-  integrations: [mdx(), react(), tailwind(), bookshop()],
+  integrations: [mdx(), react(), tailwind(), bookshop(), alpine()],
 });
