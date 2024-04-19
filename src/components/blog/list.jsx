159941692f32f0c8cdef0324a72f46c66163e4c2
tomrcc <tom@cloudcannon.com>
2024-04-19 19:43:20 +1200

    Styling footer

--- src/components/blog/list.jsx
+++ src/components/blog/list.jsx
@@ -3,7 +3,7 @@ import PostSummary from './summary';
 export default function BlogList({ posts }) {
   if (posts.length > 0) {
     return (
-      <div className="grid lg:grid-cols-2 xl:grid-cols-3 justify-between gap-5 mt-8 md:mt-16">
+      <div className="grid md:grid-cols-2 xl:grid-cols-3 justify-between gap-5 mt-8 md:mt-16">
         {posts.map((post, i) => (
           <PostSummary post={post} key={i} />
         ))}
