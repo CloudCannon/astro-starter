e3bbbed1ccc69b148163b172749f40781236ed63
tomrcc <tom@cloudcannon.com>
2024-05-02 12:58:42 +1200

    Add snippets without manual imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,20 @@ import bookshop from '@bookshop/astro-bookshop';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import alpine from '@astrojs/alpinejs';
+import remarkAutoImport from '@cloudcannon/remark-auto-import'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://muggy-fish.cloudvent.net/',
   integrations: [mdx(), react(), tailwind(), bookshop(), alpine()],
+  markdown: {
+    remarkPlugins: [
+      [
+        remarkAutoImport,
+        {
+        },
+      ],
+    ],
+    extendDefaultPlugins: true,
+  },
 });
