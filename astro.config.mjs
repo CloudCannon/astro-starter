9ed330016594e3293a1bfa72b145732b1c76c4e9
tomrcc <tom@cloudcannon.com>
2025-04-23 16:14:06 +1200

    Add default author for posts, rename default site url, fix OG title, fix import orders

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import bookshop from '@bookshop/astro-bookshop';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import alpine from '@astrojs/alpinejs';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import bookshop from "@bookshop/astro-bookshop";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import alpine from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tiny-jackal.cloudvent.net/',
+  site: "https://adjective-noun.cloudvent.net/",
   integrations: [react(), tailwind(), bookshop(), alpine(), mdx()],
 });
